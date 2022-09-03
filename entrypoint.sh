bd02a61177b75c80bc0cb3ec4285a2daad7c9f0c
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-09-03 02:09:43 +0100

    Move seediing code into the server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,6 @@
 # https://github.com/elastic/dockerfiles/blob/v7.17.6/elasticsearch/Dockerfile#L159
 /bin/tini -s -- /usr/local/bin/docker-entrypoint.sh &
 
-sleep 30
-curl -X PUT localhost:9200/products
-curl \
-  -X POST \
-  -H "content-type: application/json" \
-  --data-binary @products.json \
-  "localhost:9200/products/washers/_bulk?pretty"
-
-sleep 5
 node server &
 
 wait -n
--- server/index.js
+++ server/index.js
@@ -1,15 +1,51 @@
 const express = require("express")
 const axios = require("axios")
+const fs = require("fs").promises
+const path = require("path")
 
 const PORT = process.env.PORT ?? 3045
 
-const main = async () => {
-  try {
-    const response = await axios.get("http://localhost:9200/products/_search?pretty")
-    console.log("response.data", response.data)
-  } catch (error) {
-    console.log("error.message:", error.message)
+const range = n => Array.from(Array(n).keys())
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const waitForElasticsearchToComeUp = async () => {
+  const MAX_RETRIES = 60
+  const attempts = range(MAX_RETRIES).map(n => n + 1)
+  for (const attempt of attempts) {
+    try {
+      console.log(`[waitForElasticsearchToComeUp] attempt ${attempt}`)
+      await axios.get("http://localhost:9200")
+      console.log("[waitForElasticsearchToComeUp] Elasticsearch is up")
+      return
+    } catch (error) {
+      if (error.message.includes("ECONNREFUSED")) {
+        console.log("[waitForElasticsearchToComeUp] waiting for Elasticsearch...")
+        await delay(1000)
+      } else {
+        console.log(`[waitForElasticsearchToComeUp] ERROR: ${error.message}`)
+        return
+      }
+    }
   }
+}
+
+const createProductsIndex = async () => {
+  const fileName = path.resolve(__dirname, "..", "products.json")
+  const buffer = await fs.readFile(fileName)
+  const data = buffer.toString()
+  const config = {
+    headers: {
+      "content-type": "application/x-ndjson"
+    }
+  }
+  await axios.put("http://localhost:9200/products")
+  await axios.post("http://localhost:9200/products/washers/_bulk", data, config)
+}
+
+const main = async () => {
+  await waitForElasticsearchToComeUp()
+  await createProductsIndex()
 
   const app = express()
   app.get("*", (req, res) => {
@@ -17,7 +53,7 @@ const main = async () => {
     console.log(`${method} ${url}`)
     res.send("Hello")
   })
-  app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 }
 
 main()
