eb26c3bba917d9aea033a5c0ff3fcb8833e2c7f4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-09-03 09:56:29 +0100

    Troubleshooting deployment to render.com

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 # https://docs.docker.com/config/containers/multi-service_container/
 
+echo "Hello from entrypoint.sh"
+ls -al
+
 # https://github.com/elastic/dockerfiles/blob/v7.17.6/elasticsearch/Dockerfile#L159
-/bin/tini -s -- /usr/local/bin/docker-entrypoint.sh &
+# /bin/tini -s -- /usr/local/bin/docker-entrypoint.sh &
 
 node server &
 
