ed29ae3c36bf07f469f27f9f042ec4d8e2a11e3f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-09-03 15:49:18 +0100

    More troubleshooting (4)

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,5 @@ COPY server server
 COPY entrypoint.sh .
 RUN chmod +x entrypoint.sh
 
-# CMD ./entrypoint.sh
-CMD ["node", "server"]
+CMD ["./entrypoint.sh"]
+# CMD ["node", "server"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # ls -al
 
 # https://github.com/elastic/dockerfiles/blob/v7.17.6/elasticsearch/Dockerfile#L159
-# /bin/tini -s -- /usr/local/bin/docker-entrypoint.sh &
+/bin/tini -s -- /usr/local/bin/docker-entrypoint.sh &
 
 node server
 # node server &
